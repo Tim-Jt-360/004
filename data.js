45f56cce30096fbaef7882441ba2965ae62250fc
onofrecio <asqueroso88@gmail.com>
2025-07-24 12:32:13 +0200

    ad

--- data.js
+++ data.js
@@ -541,7 +541,7 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Zum Näfbach",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
